4e675c02e49b766b3f4e50ab25e685dd045df90f
Samuel Found <samuel.found@leo.tech>
2024-12-02 16:19:39 +0000

    Add Robot constructor tests

--- MartianRobots.Tests/RobotTests.cs
+++ MartianRobots.Tests/RobotTests.cs
@@ -2,6 +2,34 @@ namespace MartianRobots.Tests
 {
     public class RobotTests
     {
+        [Fact]
+        public void WhenDefaultConstructed_ShouldFaceNorthWithZeroZeroPosition() 
+        { 
+            //Arrange & Act
+            var robot = new Robot();
+
+            //Assert
+            Assert.Equal(0, robot.XPosition );
+            Assert.Equal(0, robot.YPosition);
+            Assert.Equal(Direction.North, robot.Direction);
+        }
+
+        [Theory]
+        [InlineData(0, 0, Direction.North)]
+        [InlineData(1, 3, Direction.East)]
+        [InlineData(2, 5, Direction.South)]
+        [InlineData(3, 8, Direction.West)]
+        public void WhenConstructed_ShouldBeInTheAssignedPostionAndDirection(int xPosition, int yPosition, Direction direction)
+        {
+            //Arrange & Act
+            var robot = new Robot(xPosition: xPosition, yPosition: yPosition, direction: direction);
+
+            //Assert
+            Assert.Equal(xPosition, robot.XPosition);
+            Assert.Equal(yPosition, robot.YPosition);
+            Assert.Equal(direction, robot.Direction);
+        }
+
         [Theory]
         [InlineData(Direction.North, 0, Direction.North)]
         [InlineData(Direction.North, 4, Direction.North)]
