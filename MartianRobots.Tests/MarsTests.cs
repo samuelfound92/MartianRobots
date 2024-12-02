693e861a07458ca9f6b8f329e444f672aa287019
Samuel Found <samuel.found@leo.tech>
2024-12-02 18:21:52 +0000

    Add Mars class and tests

---
+++ MartianRobots.Tests/MarsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianRobots.Tests
+{
+    public class MarsTests
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(20, 30)]
+        public void WhenConstructed_ShouldHaveCorrectDimensions(int x_UpperLimit, int y_UpperLimit) 
+        {
+            //Arrange
+            var mars = new Mars(x_UpperLimit, y_UpperLimit);
+
+            //Assert
+            Assert.Equal(x_UpperLimit, mars.X_UpperLimit);
+            Assert.Equal(y_UpperLimit, mars.Y_UpperLimit);
+        }
+    }
+}
---
+++ MartianRobots/Mars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MartianRobots
+{
+    public class Mars(int width, int height)
+    {
+        public const int X_LowerLimit = 0;
+        public const int Y_LowerLimit = 0;
+
+        public int X_UpperLimit { get; set; } = width;
+        public int Y_UpperLimit { get; set; } = height;
+    }
+}
